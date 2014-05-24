4717bc584da9117ea7103b87b12552503ae14c9b
speed-of-light <speed.of.lightt@gmail.com>
2014-05-24 20:22:17 +0800

    Follow PEP8, Pyflask

--- lib/_exp/matching.py
+++ lib/_exp/matching.py
@@ -5,10 +5,11 @@ import pandas as pd
 import numpy as np
 from sklearn import mixture
 from summary import Summary
-from feats   import Feats
-from ..data  import *
+from feats import Feats
+from ..data import Video, PdfSlider
 from ..handy import HandyTimer as ht
-from ..exp   import ExpCommon
+from ..exp import ExpCommon
+
 
 class Matcher(ExpCommon):
   def __init__(self, root, name):
@@ -17,71 +18,88 @@ class Matcher(ExpCommon):
     """
     ExpCommon.__init__(self, root, name)
 
+  def _match_info(self, sid, fid, kalg, dalg, mtype, thres):
+    rs = "sid:{}, fid:{}, mod:{}-{}, mtype:{},thres:{}" \
+         .format(sid, fid, kalg, dalg, mtype, thres)
+    return rs
+
   def set_match(self, fids=[], mtype="BruteForce", thres=.9,
-      mod=dict(kp_algo='FAST', des_algo='SIFT')):
-    sr = self.root; sn = self.name
-    ff = Feats(sr, sn); isl = ff.o_slides(gray=True, resize=True)
-    vv = Video(sr, sn); ivl = vv.get_frames(ids=fids, gray=True)
-    sfs = ff.feats_set(isl, mod=mod); vfs = ff.feats_set(ivl, mod=mod)
+                mod=dict(kp_algo='FAST', des_algo='SIFT')):
+    sr = self.root
+    sn = self.name
+    ff = Feats(sr, sn)
+    isl = ff.o_slides(gray=True, resize=True)
+    vv = Video(sr, sn)
+    ivl = vv.get_frames(ids=fids, gray=True)
+    sfs = ff.feats_set(isl, mod=mod)
+    vfs = ff.feats_set(ivl, mod=mod)
     res = []
     for qf in vfs:
       frs = []
       for tf in sfs:
-        infostr= "sid:{}, fid:{}, mod:{}-{}, mtype:{},thres:{}".format(tf['idx'], qf['idx'], mod['kp_algo'], mod['des_algo'], mtype, thres)
-        self.log.info(infostr)
+        _info = self._match_info(tf['idx'], qf['idx'], mod['kp_algo'],
+                                 mod['des_algo'], mtype, thres)
+        self.log.info(_info)
         with ht(verbose=0) as ts:
           mr = self._match_desc(qf['des'], tf['des'], mtype, thres)
-        mt = ts.msecs+tf['ts']+qf['ts']
-        dr = dict(sid=tf['idx'], fid=qf['idx'], ts=mt, mr=mr)
-        frs.append(dr)
-      ds = self._dist_stat(frs)
-      vr = self._voting(ds)
-      res.append(dict(feats=frs, vr=vr))
-    return res, sfs, vfs
+          mt = ts.msecs+tf['ts']+qf['ts']
+          dr = dict(sid=tf['idx'], fid=qf['idx'], ts=mt, mr=mr)
+          frs.append(dr)
+          ds = self._dist_stat(frs)
+          vr = self._voting(ds)
+          res.append(dict(feats=frs, vr=vr))
+          return res, sfs, vfs
 
   def _match_desc(self, dquery, dtrain, mtype="BruteForce", thres=.5):
     """
     BruteForce-L1 BruteForce-Hamming BruteForce-Hamming(2) FlannBased
     """
     mat = cv2.DescriptorMatcher_create(mtype)
-    #mre = mat.match(dquery, dtrain)
+    # mre = mat.match(dquery, dtrain)
     mra = mat.knnMatch(dquery, dtrain, k=2)
     mre = []
-    for m,n in mra:
+    for m, n in mra:
       if m.distance < n.distance*thres:
         mre.append(m)
-    mra = []
-    for re in mre:
-      mra.append([re.queryIdx, re.trainIdx, re.imgIdx, re.distance])
-    if len(mra) is 0:
-      return pd.DataFrame(columns=['qix', 'tix', 'iix', 'dt'])
-    else:
-      return pd.DataFrame(mra, columns=['qix', 'tix', 'iix', 'dt'])
+        mra = []
+        for re in mre:
+          mra.append([re.queryIdx, re.trainIdx, re.imgIdx, re.distance])
+          if len(mra) is 0:
+            return pd.DataFrame(columns=['qix', 'tix', 'iix', 'dt'])
+          else:
+            return pd.DataFrame(mra, columns=['qix', 'tix', 'iix', 'dt'])
 
   def _filter_matches(self, matches, thres=.5):
     mre = []
-    for m,n in matches:
+    for m, n in matches:
       if m.distance < n.distance*thres:
         mre.append(m)
-    return mre
+        return mre
 
   def _pickle_matches(self, matches):
     mra = []
     for m in matches:
       mra.append([m.queryIdx, m.trainIdx, m.imgIdx, m.distance])
-    return mra
+      return mra
+
+  def _image_pair(self, fid, sid):
+    sr = self.root
+    sn = self.name
+    vv = Video(sr, sn)
+    fi = vv.get_frames([fid]).next()
+    ss = PdfSlider(sn, sr)
+    si = ss.get_slides([sid], gray=True, resize=True).next()
+    return fi, si
 
   def single_match(self, fid, sid, mtype="BruteForce",
-      mod=dict(kp_algo='FAST', des_algo='SIFT')):
+                   mod=dict(kp_algo='FAST', des_algo='SIFT')):
     """
     BruteForce-L1 BruteForce-Hamming BruteForce-Hamming(2) FlannBased
     """
-    sr = self.root; sn = self.name
-    vv = Video(sr, sn); vi = vv.get_frames([fid]).next()
-    ss = PdfSlider(sn, sr); ff = Feats(sr, sn);
-    si = ss.get_slides( [sid], gray=True, resize=True).next()
-    sif = ff.feats_set( [si], mod=mod)[0]
-    vif = ff.feats_set( [vi], mod=mod)[0]
+    fimg, simg = self._image_pair(fid, sid)
+    ff = Feats(self.root, self.name)
+    sif = ff.feats_set([simg], mod=mod)[0]
+    vif = ff.feats_set([fimg], mod=mod)[0]
     mat = cv2.DescriptorMatcher_create(mtype)
     mra = mat.knnMatch(sif['des'], vif['des'], k=2)
     mra = self._filter_matches(mra, .9)
@@ -91,52 +109,60 @@ class Matcher(ExpCommon):
     else:
       mra = self._pickle_matches(mra)
       res = pd.DataFrame(mra, columns=col)
-    return dict(matches=res, sif=sif, vif=vif)
+      return dict(matches=res, sif=sif, vif=vif)
 
   def _voting(self, df):
     vk = []
     for cc in df.columns:
-      if cc in ['sid', 'fid']: continue
+      if cc in ['sid', 'fid']:
+        continue
       vr = df[df[cc].eq(df[cc].min())]
-      if len(vr) > 0: vk.append(int(vr.iloc[0].sid))
-      else: vk.append(None)
-    return vk
+      if len(vr) > 0:
+        vk.append(int(vr.iloc[0].sid))
+      else:
+        vk.append(None)
+        return vk
 
   def _dist_stat(self, fres):
     ary = []
     for fk in fres:
       mr = fk['mr']
       m3 = mr[mr.dt.gt(mr.dt.quantile(.9))].dt.mean()
-      mm = mr[mr.dt.lt(mr.dt.quantile(.8)) & mr.dt.gt(mr.dt.quantile(.2))].dt.mean()
+      mm = mr[mr.dt.lt(mr.dt.quantile(.8)) &
+              mr.dt.gt(mr.dt.quantile(.2))].dt.mean()
       m1 = mr[mr.dt.lt(mr.dt.quantile(.1))].dt.mean()
       M = mr.dt.max()
       m = mr.dt.min()
       ary.append([fk['fid'], fk['sid'], m3, mm, m1, m1/m3, M, m, m/M])
-    cols = ['fid', 'sid', 'm3', 'mm', 'm1', 'm31', 'M', 'm', 'mM']
-    return pd.DataFrame(ary, columns=cols)
+      cols = ['fid', 'sid', 'm3', 'mm', 'm1', 'm31', 'M', 'm', 'mM']
+      return pd.DataFrame(ary, columns=cols)
 
   def _plot_match(self, qimg, qkp, timg, tkp, match):
-    h1, w1 = qimg.shape[:2]; h2, w2 = timg.shape[:2]
+    h1, w1 = qimg.shape[:2]
+    h2, w2 = timg.shape[:2]
     view = np.zeros((max(h1, h2), w1 + w2, 3), np.uint8)
-    view[:h1, :w1] = qimg; view[:h2, w1:] = timg
+    view[:h1, :w1] = qimg
+    view[:h2, w1:] = timg
     for mi in match.index:
-      tix = int(match.ix[mi]['tix']); qix = int(match.ix[mi]['qix'])
+      tix = int(match.ix[mi]['tix'])
+      qix = int(match.ix[mi]['qix'])
       color = tuple([np.random.randint(0, 255) for _ in xrange(3)])
       qmp = (int(qkp[qix].pt[0]), int(qkp[qix].pt[1]))
       tmp = (int(tkp[tix].pt[0] + w1), int(tkp[tix].pt[1]))
       cv2.line(view, qmp, tmp, color)
-    return view
+      return view
 
   def _plot_single_match(self, fid, fkp, sid, skp, mresult):
     vv = Video(self.root, self.name)
     fimg = vv.get_frames(ids=[fid]).next()['img']
     spm = self.slides_path(size='big')
     simg = cv2.imread('{}/{:03d}.jpg'.format(spm, sid))
-    su = Summary(); sin = su.info(self.root, self.name).iloc[0]
+    su = Summary()
+    sin = su.info(self.root, self.name).iloc[0]
     simg = cv2.resize(simg, (sin.v_width, sin.v_height))
-    vimg = self.plot_match( fimg, fkp, simg, skp, mresult)
+    vimg = self.plot_match(fimg, fkp, simg, skp, mresult)
     return vimg
-    #cv2.imwrite('jpg/test.jpg', vimg)
+  # cv2.imwrite('jpg/test.jpg', vimg)
 
   def _reorder(self, old, li=[]):
     """
@@ -149,9 +175,9 @@ class Matcher(ExpCommon):
     tmp = old[:]
     tmp.sort()
     bb = {}
-    for i,t in enumerate(tmp):
+    for i, t in enumerate(tmp):
       bb[i] = old.index(t)
-    return [bb[ll] for ll in li]
+      return [bb[ll] for ll in li]
 
   def _basic_result(self, data, mm=2):
     clf = mixture.GMM(n_components=mm, covariance_type='full')
@@ -162,30 +188,33 @@ class Matcher(ExpCommon):
   def regroup(self, df, smres):
     """
     `df` Ground-truths dataframe, columns are:
-      'fid', 'sid', 'slide_type', 'cam_status'
+    'fid', 'sid', 'slide_type', 'cam_status'
     `smres` Comes from method set_match and its res value
     """
     hita = []
     for r in smres:
-      fid = r['feats'][0]['fid'] # 0 is ok, all the same
+      fid = r['feats'][0]['fid']  # 0 is ok, all the same
       qry = df[(df['fid'] <= fid)]
       if len(qry) > 0:
         qv = qry.values[-1][1]
         mr = r['feats'][qv-1]['mr']
         tmean = mr[mr.dt.lt(mr.dt.quantile(.1))].dt.mean()
-        if qv > 0: # has sid
+        if qv > 0:  # has sid
           hita.append([fid, qv, r['vr'], tmean, None, tmean])
-        elif len(r['feats'][qv-1]['mr']) > 0: # has matched result
+        elif len(r['feats'][qv-1]['mr']) > 0:  # has matched result
           hita.append([fid, qv, r['vr'], None, tmean, tmean])
-      else:
-        print 'never goes here'
-    hitf = pd.DataFrame(hita, columns=['fid', 'sid', 'vr', 'hit', 'nonhit', 'hita'])
-    hitf = hitf.set_index('fid')
-    hitf['pr'] = self._basic_result(hitf.hita.values, 2)
-    return hitf
-    #self.plot_hitf(hitf, show=[1482, 16427, 28957, 35469, 46755, 47461, 66385, 70360])
+        else:
+          print 'never goes here'
+          hitf = pd.DataFrame(hita,
+                              columns=['fid', 'sid', 'vr', 'hit',
+                                       'nonhit', 'hita'])
+          hitf = hitf.set_index('fid')
+          hitf['pr'] = self._basic_result(hitf.hita.values, 2)
+          return hitf
+     # self.plot_hitf(hitf,
+     #         show=[1482, 16427, 28957, 35469, 46755, 47461,66385,70360])
 
-  #binary
+  # binary
   def tmp(self):
     """
     http://opencv-python-tutroals.readthedocs.org/en/latest/py_tutorials/py_feature2d/py_brief/py_brief.html?highlight=brief
@@ -202,7 +231,7 @@ class Matcher(ExpCommon):
   def plot_hitf(self, data, show=[], predict=False, hints=False):
     """
     data(dataframe) should contain columns:
-      ['fid', 'sid', 'vr', 'hit', 'nonhit', 'hita']
+    ['fid', 'sid', 'vr', 'hit', 'nonhit', 'hita']
     vr: voted result ( not necessary)
     hit: similarity or confidence for true
     nonhit: similarity or confidence for false(wrong match)
@@ -217,109 +246,127 @@ class Matcher(ExpCommon):
       self.name = name
 
     def _stich_imgs(self, left, right):
-      h1, w1 = left.shape[:2]; h2, w2 = right.shape[:2]
+      h1, w1 = left.shape[:2]
+      h2, w2 = right.shape[:2]
       view = np.zeros((max(h1, h2), w1 + w2, 3), np.uint8)
-      view[:h1, :w1] = left; view[:h2, w1:] = right
+      view[:h1, :w1] = left
+      view[:h2, w1:] = right
       return view
 
     def _ano_img_box(self, ax, vimg, simg, hitc, sid):
       hv = 0 if math.isnan(hitc.hit) else hitc.hit
       hv = hv if math.isnan(hitc.nonhit) else hitc.nonhit
       sip = 0.1 if sid % 2 is 0 else 0.9
-      oft = ofb.OffsetImage(self._stich_imgs(simg,vimg[:,:,[2,1,0]]), zoom=0.1)
-      ab = ofb.AnnotationBbox(oft, (hitc.name, hv), xycoords='data', xybox=(hitc.name, sip),
-        boxcoords=("data", "axes fraction"), bboxprops=dict(boxstyle='round,pad=0.1', ec='g'),
-        arrowprops=dict(arrowstyle="->", color='g'))
+      stimg = self._stich_imgs(simg, vimg[:, :, [2, 1, 0]])
+      oft = ofb.OffsetImage(stimg, zoom=0.1)
+      ab = ofb.AnnotationBbox(oft, (hitc.name, hv), xycoords='data',
+                              xybox=(hitc.name, sip),
+                              boxcoords=("data", "axes fraction"),
+                              bboxprops=dict(boxstyle='round,pad=0.1', ec='g'),
+                              arrowprops=dict(arrowstyle="->", color='g'))
       ax.add_artist(ab)
 
     def _ano_fid_note(self, ax, hitf):
       hM = hitf['hit'].max()
       for fid in hitf.index:
-        hv = hitf.ix[fid].hit #and hitf.ix[fid].nonhit
-        hva = hM*(fid%5)*0.05
-        hva = hva if fid%2 is 0 else -hva
+        hv = hitf.ix[fid].hit  # and hitf.ix[fid].nonhit
+        hva = hM*(fid % 5)*0.05
+        hva = hva if fid % 2 is 0 else -hva
         oft = ofb.TextArea("{}[{}]".format(fid, hitf.ix[fid].sid))
-        ab = ofb.AnnotationBbox(oft, (fid, hv), xycoords='data', xybox=(fid, hv+hva),
-          boxcoords=("data"), bboxprops=dict(boxstyle='round,pad=0.1', ec='g'),
-          arrowprops=dict(arrowstyle="->", color='darkblue'))
+        ab = ofb.AnnotationBbox(oft, (fid, hv), xycoords='data',
+                                xybox=(fid, hv+hva), boxcoords=("data"),
+                                bboxprops=dict(boxstyle='round,pad=0.1',
+                                               ec='g'),
+                                arrowprops=dict(arrowstyle="->",
+                                                color='darkblue'))
         ax.add_artist(ab)
 
     def _add_range_span(self, ax, mean, std, xpos, color='g'):
       ax.axhline(mean)
       ax.axhspan(mean-std, mean+std, facecolor=color, alpha=0.5)
       font = dict(family='serif', color='blue', weight='normal', size=16)
-      ax.text( xpos, mean+3, "{:.3f}".format(mean), fontdict=font)
+      ax.text(xpos, mean+3, "{:.3f}".format(mean), fontdict=font)
 
     def plot_hitf(self, data, **kwargs):
       """
       data(dataframe) should contain columns:
-        ['fid', 'sid', 'vr', 'hit', 'nonhit', 'hita']
+      ['fid', 'sid', 'vr', 'hit', 'nonhit', 'hita']
       vr: voted result ( not necessary)
       hit: similarity or confidence for true
       nonhit: similarity or confidence for false(wrong match)
       hita: combined hit and nonhit result withou nan/none
       """
-      hints = kwargs['hints']; show = kwargs['show']
+      hints = kwargs['hints']
+      show = kwargs['show']
       predict = kwargs['predict']
       # Hit
       hhs = data[data['hit'].notnull()].hit
       ax = hhs.plot(style='go', figsize=(18, 9), alpha=0.6)
-      self._add_range_span(ax, hhs.mean(), hhs.std(), data.index.max()*0.01, color='limegreen')
-      if hints: self._ano_fid_note(ax, data[data['hit'].notnull()])
+      self._add_range_span(ax, hhs.mean(), hhs.std(), data.index.max()*0.01,
+                           color='limegreen')
+      if hints:
+        self._ano_fid_note(ax, data[data['hit'].notnull()])
       # Not hit
       hhs = data[data['nonhit'].notnull()].nonhit
       ax = hhs.plot(style='rx')
-      self._add_range_span(ax, hhs.mean(), hhs.std(), data.index.max()*0.01, color='pink')
+      self._add_range_span(ax, hhs.mean(), hhs.std(), data.index.max()*0.01,
+                           color='pink')
       # Predicted
       if predict:
         hhs = data[(data.pr == 0)].hita
         ax = hhs.plot(ms=15, color='#7CFC00', ls='', marker='p', alpha=.3)
         hhs = data[(data.pr == 1)].hita
         ax = hhs.plot(ms=15, color='#ff69b4', ls='', marker='h', alpha=.3)
-      # Others
-      if len(show) > 0:
-        vid = Video(self.root, self.name)
-        slid = PdfSlider(self.name, self.root)
-        for si, fi in enumerate(show):
-          vimg = vid.get_frames([fi]).next()['img']
-          simg = slid.get_slide(data.ix[fi].sid, resize=True)
-          self._ano_img_box(ax, vimg, simg, data.ix[fi], si)
-      return ax
+        # Others
+        if len(show) > 0:
+          vid = Video(self.root, self.name)
+          slid = PdfSlider(self.name, self.root)
+          for si, fi in enumerate(show):
+            vimg = vid.get_frames([fi]).next()['img']
+            simg = slid.get_slide(data.ix[fi].sid, resize=True)
+            self._ano_img_box(ax, vimg, simg, data.ix[fi], si)
+            return ax
 
-  def ransac(self, res):
+  def ransac(self, fid, sid, res, plot=False):
     ra = self.Ransac()
-    ra.compute()
+    if plot:
+      fimg, simg = self._image_pair(fid, sid)
+    else:
+      ra.compute()
 
   class Ransac():
     def __init__(self):
       pass
 
     def _matched_points(self, kps, mps):
-      rr = [ kps[m].pt for m in mps ]
-      return np.float32( rr ).reshape(-1,1,2)
+      rr = [kps[m].pt for m in mps]
+      return np.float32(rr).reshape(-1, 1, 2)
 
     def compute(self, data, min_matches=10):
       """
       data is a dict contained columns:
-        [ 'kp_train', 'kp_test', 'matches']
+      [ 'kp_train', 'kp_test', 'matches']
       Methods:
         0 - a regular method using all the points
         CV_RANSAC - RANSAC-based robust method
         CV_LMEDS - Least-Median robust method
       """
       good = data['matches']
-      kpq = data['sif']['kps']; kpt = data['vif']['kps']
+      kpq = data['sif']['kps']
+      kpt = data['vif']['kps']
       if len(good) >= min_matches:
-        qpts = self._matched_points( kpq, good.qix )
-        tpts = self._matched_points( kpt, good.tix )
-        M, mask = cv2.findHomography(qpts, tpts, method=cv2.RANSAC, ransacReprojThreshold=5.0)
+        qpts = self._matched_points(kpq, good.qix)
+        tpts = self._matched_points(kpt, good.tix)
+        M, mask = cv2.findHomography(qpts, tpts, method=cv2.RANSAC,
+                                     ransacReprojThreshold=5.0)
         return M, mask
       return None
 
-    def get_bound_img(self, fimg, timg, homo):
-      h,w,z = fimg.shape
-      bnd = np.float32([ [0,0], [0,h-1], [w-1,h-1], [w-1,0] ]).reshape(-1,1,2)
+    def get_bound_img(self, fid, sid, homo):
+      fimg, simg = self._image_pair(fid, sid)
+      h, w, z = fimg.shape
+      bnd = np.float32([[0, 0], [0, h-1], [w-1, h-1], [w-1, 0]]). \
+          reshape(-1, 1, 2)
       bound = cv2.perspectiveTransform(bnd, homo)
-      rimg = cv2.polylines( timg, [np.int32(bound)], True, (0, 255, 0), 3)
+      rimg = cv2.polylines(simg, [np.int32(bound)], True, (0, 255, 0), 3)
       return rimg
-
