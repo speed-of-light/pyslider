e1e41713f580f9c100b5b66835a42b3a29238896
speed-of-light <speed.of.lightt@gmail.com>
2014-05-26 10:21:02 +0800

    Add plot_id_match

--- lib/_exp/matching.py
+++ lib/_exp/matching.py
@@ -4,7 +4,6 @@ import cv2
 import pandas as pd
 import numpy as np
 from sklearn import mixture
-from summary import Summary
 from feats import Feats
 from ..data import Video, PdfSlider
 from ..handy import HandyTimer as ht
@@ -39,61 +38,34 @@ class Ransac():
           return M, mask
         return None
 
-    def get_bound_img(self, fid, sid, homo):
+    def get_bound_img(self, fimg, simg, homo):
         """
         Return homographic array of image result
         Params:
-            fid: id of frames
-            sid: id of slide
+            fimg: img array of frames
+            simg: img array of slide
             homo: transformation matrix (usually 9*9)
         """
-        fimg, simg = self._image_pair(fid, sid)
         h, w, z = fimg.shape
         bnd = np.float32([[0, 0], [0, h-1], [w-1, h-1], [w-1, 0]]). \
             reshape(-1, 1, 2)
         bound = cv2.perspectiveTransform(bnd, homo)
-        rimg = cv2.polylines(simg, [np.int32(bound)], True, (0, 255, 0), 3)
+        rimg = simg[:]
+        cv2.polylines(rimg, [np.int32(bound)], True, (0, 255, 0), 3)
         return rimg
 
 
 class Matcher(ExpCommon):
     def __init__(self, root, name):
-      """
-      Get effective frame lists from prepared data
-      """
-      ExpCommon.__init__(self, root, name)
+        """
+        Get effective frame lists from prepared data
+        """
+        ExpCommon.__init__(self, root, name)
 
     def _match_info(self, sid, fid, kalg, dalg, mtype, thres):
       rs = "sid:{}, fid:{}, mod:{}-{}, mtype:{},thres:{}" \
            .format(sid, fid, kalg, dalg, mtype, thres)
       return rs
-
-    def set_match(self, fids=[], mtype="BruteForce", thres=.9,
-                  mod=dict(kp_algo='FAST', des_algo='SIFT')):
-      sr = self.root
-      sn = self.name
-      ff = Feats(sr, sn)
-      isl = ff.o_slides(gray=True, resize=True)
-      vv = Video(sr, sn)
-      ivl = vv.get_frames(ids=fids, gray=True)
-      sfs = ff.feats_set(isl, mod=mod)
-      vfs = ff.feats_set(ivl, mod=mod)
-      res = []
-      for qf in vfs:
-        frs = []
-        for tf in sfs:
-          _info = self._match_info(tf['idx'], qf['idx'], mod['kp_algo'],
-                                   mod['des_algo'], mtype, thres)
-          self.log.info(_info)
-          with ht(verbose=0) as ts:
-            mr = self._match_desc(qf['des'], tf['des'], mtype, thres)
-            mt = ts.msecs+tf['ts']+qf['ts']
-            dr = dict(sid=tf['idx'], fid=qf['idx'], ts=mt, mr=mr)
-            frs.append(dr)
-            ds = self._dist_stat(frs)
-            vr = self._voting(ds)
-            res.append(dict(feats=frs, vr=vr))
-            return res, sfs, vfs
 
     def _match_desc(self, dquery, dtrain, mtype="BruteForce", thres=.5):
         """
@@ -127,34 +99,14 @@ class Matcher(ExpCommon):
         mra.append([m.queryIdx, m.trainIdx, m.imgIdx, m.distance])
         return mra
 
-    def _image_pair(self, fid, sid):
+    def _image_pair(self, fid, sid, gray=True):
       sr = self.root
       sn = self.name
       vv = Video(sr, sn)
-      fi = vv.get_frames([fid]).next()
+      fi = vv.get_frames([fid], gray=gray).next()
       ss = PdfSlider(sn, sr)
-      si = ss.get_slides([sid], gray=True, resize=True).next()
+      si = ss.get_slides([sid], gray=gray, resize=True).next()
       return fi, si
-
-    def single_match(self, fid, sid, mtype="BruteForce",
-                     mod=dict(kp_algo='FAST', des_algo='SIFT')):
-        """
-        BruteForce-L1 BruteForce-Hamming BruteForce-Hamming(2) FlannBased
-        """
-        fimg, simg = self._image_pair(fid, sid)
-        ff = Feats(self.root, self.name)
-        sif = ff.feats_set([simg], mod=mod)[0]
-        vif = ff.feats_set([fimg], mod=mod)[0]
-        mat = cv2.DescriptorMatcher_create(mtype)
-        mra = mat.knnMatch(sif['des'], vif['des'], k=2)
-        mra = self._filter_matches(mra, .9)
-        col = ['qix', 'tix', 'iix', 'dt']
-        if len(mra) is 0:
-          res = pd.DataFrame(columns=col)
-        else:
-          mra = self._pickle_matches(mra)
-          res = pd.DataFrame(mra, columns=col)
-          return dict(matches=res, sif=sif, vif=vif)
 
     def _voting(self, df):
       vk = []
@@ -182,33 +134,6 @@ class Matcher(ExpCommon):
         cols = ['fid', 'sid', 'm3', 'mm', 'm1', 'm31', 'M', 'm', 'mM']
         return pd.DataFrame(ary, columns=cols)
 
-    def _plot_match(self, qimg, qkp, timg, tkp, match):
-      h1, w1 = qimg.shape[:2]
-      h2, w2 = timg.shape[:2]
-      view = np.zeros((max(h1, h2), w1 + w2, 3), np.uint8)
-      view[:h1, :w1] = qimg
-      view[:h2, w1:] = timg
-      for mi in match.index:
-        tix = int(match.ix[mi]['tix'])
-        qix = int(match.ix[mi]['qix'])
-        color = tuple([np.random.randint(0, 255) for _ in xrange(3)])
-        qmp = (int(qkp[qix].pt[0]), int(qkp[qix].pt[1]))
-        tmp = (int(tkp[tix].pt[0] + w1), int(tkp[tix].pt[1]))
-        cv2.line(view, qmp, tmp, color)
-        return view
-
-    def _plot_single_match(self, fid, fkp, sid, skp, mresult):
-      vv = Video(self.root, self.name)
-      fimg = vv.get_frames(ids=[fid]).next()['img']
-      spm = self.slides_path(size='big')
-      simg = cv2.imread('{}/{:03d}.jpg'.format(spm, sid))
-      su = Summary()
-      sin = su.info(self.root, self.name).iloc[0]
-      simg = cv2.resize(simg, (sin.v_width, sin.v_height))
-      vimg = self.plot_match(fimg, fkp, simg, skp, mresult)
-      return vimg
-    # cv2.imwrite('jpg/test.jpg', vimg)
-
     def _reorder(self, old, li=[]):
         """
         Generate reordered list
@@ -229,6 +154,53 @@ class Matcher(ExpCommon):
       clf.fit(data)
       pr = clf.predict(data)
       return np.array(self._reorder(clf.means_.tolist(), pr))
+
+    def single_match(self, fid, sid, mtype="BruteForce",
+                     mod=dict(kp_algo='FAST', des_algo='SIFT')):
+        """
+        BruteForce-L1 BruteForce-Hamming BruteForce-Hamming(2) FlannBased
+        """
+        fimg, simg = self._image_pair(fid, sid)
+        ff = Feats(self.root, self.name)
+        sif = ff.feats_set([simg], mod=mod)[0]
+        vif = ff.feats_set([fimg], mod=mod)[0]
+        mat = cv2.DescriptorMatcher_create(mtype)
+        mra = mat.knnMatch(sif['des'], vif['des'], k=2)
+        mra = self._filter_matches(mra, .9)
+        col = ['qix', 'tix', 'iix', 'dt']
+        if len(mra) is 0:
+          res = pd.DataFrame(columns=col)
+        else:
+          mra = self._pickle_matches(mra)
+          res = pd.DataFrame(mra, columns=col)
+          return dict(matches=res, sif=sif, vif=vif)
+
+    def set_match(self, fids=[], mtype="BruteForce", thres=.9,
+                  mod=dict(kp_algo='FAST', des_algo='SIFT')):
+      sr = self.root
+      sn = self.name
+      ff = Feats(sr, sn)
+      isl = ff.o_slides(gray=True, resize=True)
+      vv = Video(sr, sn)
+      ivl = vv.get_frames(ids=fids, gray=True)
+      sfs = ff.feats_set(isl, mod=mod)
+      vfs = ff.feats_set(ivl, mod=mod)
+      res = []
+      for qf in vfs:
+        frs = []
+        for tf in sfs:
+          _info = self._match_info(tf['idx'], qf['idx'], mod['kp_algo'],
+                                   mod['des_algo'], mtype, thres)
+          self.log.info(_info)
+          with ht(verbose=0) as ts:
+            mr = self._match_desc(qf['des'], tf['des'], mtype, thres)
+            mt = ts.msecs+tf['ts']+qf['ts']
+            dr = dict(sid=tf['idx'], fid=qf['idx'], ts=mt, mr=mr)
+            frs.append(dr)
+            ds = self._dist_stat(frs)
+            vr = self._voting(ds)
+            res.append(dict(feats=frs, vr=vr))
+            return res, sfs, vfs
 
     def regroup(self, df, smres):
         """
@@ -277,12 +249,46 @@ class Matcher(ExpCommon):
         """
         pass
 
-    def ransac(self, fid, sid, res, plot=False):
-      ra = self.Ransac()
-      if plot:
-        fimg, simg = self._image_pair(fid, sid)
-      else:
-        ra.compute()
+    def ransac(self, res):
+        ra = Ransac()
+        ra.compute(res)
+
+    def plot_ransac(self, fid, sid, res):
+        ra = Ransac()
+        homo, mask = ra.compute(res)
+        fp, sp = self._image_pair(fid, sid, gray=False)
+        himg = ra.get_bound_img(fp['img'], sp['img'], homo)
+        return himg
+
+    def plot_img_match(self, qimg, qkp, timg, tkp, match):
+      h1, w1 = qimg.shape[:2]
+      h2, w2 = timg.shape[:2]
+      view = np.zeros((max(h1, h2), w1 + w2, 3), np.uint8)
+      view[:h1, :w1] = qimg
+      view[:h2, w1:] = timg
+      for mi in match.index:
+        tix = int(match.ix[mi]['tix'])
+        qix = int(match.ix[mi]['qix'])
+        color = tuple([np.random.randint(0, 255) for _ in xrange(3)])
+        qmp = (int(qkp[qix].pt[0]), int(qkp[qix].pt[1]))
+        tmp = (int(tkp[tix].pt[0] + w1), int(tkp[tix].pt[1]))
+        cv2.line(view, qmp, tmp, color)
+        return view
+
+    def plot_id_match(self, fin, sin, fkp, skp, mresult):
+        if isinstance(sin, int):
+            ss = PdfSlider(self.name, self.root)
+            simg = ss.get_slides([sin], gray=False, resize=True).next()['img']
+        else:
+            simg = sin
+        if isinstance(fin, int):
+            vv = Video(self.root, self.name)
+            fimg = vv.get_frames([fin], gray=False).next()['img']
+        else:
+            fimg = fin
+        vimg = self.plot_img_match(fimg, fkp, simg, skp, mresult)
+        return vimg
+    # cv2.imwrite('jpg/test.jpg', vimg)
 
     def plot_hitf(self, data, show=[], predict=False, hints=False):
         """
@@ -295,9 +301,6 @@ class Matcher(ExpCommon):
         """
         mp = self.Plotter(self.root, self.name)
         mp.plot_matches(data, show=show, predict=predict, hints=hints)
-
-    def plot_ransac(self, ):
-        pass
 
     class Plotter():
         def __init__(self, root, name):
