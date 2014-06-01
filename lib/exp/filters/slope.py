d3a2e08466cc4807a0b67f5fd195d53090312595
speed-of-light <speed.of.lightt@gmail.com>
2014-06-01 13:50:32 +0800

    Modify result to generate `keep` column for #18

--- lib/exp/filters/slope.py
+++ lib/exp/filters/slope.py
@@ -8,7 +8,7 @@ class Slope(KpFilter):
     def __init__(self):
         pass
 
-    def _slope(self, spt, fpt):
+    def __slope(self, spt, fpt):
         """
         Get slope from 2 points
         """
@@ -16,7 +16,7 @@ class Slope(KpFilter):
         dy = fpt[1] - spt[1]
         return dy*1.0/dx
 
-    def _slopes(self, skps, fkps, good):
+    def __slopes(self, skps, fkps, good):
         """
         Getting matches slope list
         skp: slide keypoints
@@ -24,27 +24,39 @@ class Slope(KpFilter):
         """
         fpt = fkps[int(good.tix)].pt
         spt = skps[int(good.qix)].pt
-        ss = self._slope(spt, fpt)
+        ss = self.__slope(spt, fpt)
         return ss
+
+    def __keeper(self, match, bot, top):
+        val = -1
+        if match.slope >= bot and match.slope <= top:
+            val = 1
+        else:
+            val = 0
+        return val
 
     def get_slopes(self, ret):
         """
         ret is dict contains: [matches, sif, vif]
         """
         good = ret['matches']
-        good['slope'] = good.apply(lambda matches: self._slopes(
+        good['slope'] = good.apply(lambda matches: self.__slopes(
             ret['sif']['kps'], ret['vif']['kps'], matches), axis=1)
         return good
 
     def result(self, good, sigma=.5):
         """
         sigma: tolerance of input slopes
+        Return original dataframe with a `keep` column to indicate keep data
+        or not.
         """
         smean = good.slope.mean()
         sstd = good.slope.std()*sigma
         bot = smean - sstd
         top = smean + sstd
-        return good[(good['slope'] >= bot) & (good['slope'] <= top)]
+        good['keep'] = good.apply(lambda row:
+                                  self.__keeper(row, bot, top), axis=1)
+        return good
 
     # TODO: add gridize filter result
     def gridise(self):
