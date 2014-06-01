a06abf8f7c68bddc251285939c5a32e56dd86e8a
speed-of-light <speed.of.lightt@gmail.com>
2014-06-01 12:14:08 +0800

    Rename PdfSlider to PdfReader, and simplify funtions, seperate to summary.Slider

--- lib/data.py
+++ lib/data.py
@@ -125,7 +125,6 @@ class Video:
     return df[df.isum.gt(iqr)]
 
 
-from lib._exp.summary import Summary
 from PyPDF2 import PdfFileReader
 import shutil
 # from wand.image import Image, Color
@@ -133,7 +132,7 @@ from pgmagick import Image
 from PIL import Image as pimg
 
 
-class PdfSlider():
+class PdfReader(object):
   """
   Author: speed-of-light
   Purpose: Operations on pdf files
@@ -198,63 +197,3 @@ class PdfSlider():
           img.write("{}.jpg".format(slid))
       # break
       # self.png_jpg('test.png')
-
-  def _blank_slide(self):
-    sp = self.slides_path()
-    img = cv2.imread("{}/{:03d}.jpg".format(sp, 1))
-    img[:] = (255, 255, 255)  # fill white
-    # red cross
-    red = (0, 0, 255)
-    line_width = int(0.1*img.shape[0])
-    topL = (0, 0)
-    botR = (img.shape[1], img.shape[0])
-    topR = (0, img.shape[0])
-    botL = (img.shape[1], 0)
-    cv2.line(img, topL, botR, red, line_width)
-    cv2.line(img, topR, botL, red, line_width)
-    return img
-
-  def _is_valid_sid(self, index, count):
-    return (index > 0 and index < count+1)
-
-  def _img_path(self, root, idx):
-    return "{}/{:03d}.jpg".format(root, idx)
-
-  def get_slides(self, ids=[], gray=False, resize=None):
-    """
-    Get slide images collection
-    """
-    sp = self.slides_path(size='big')
-    su = Summary()
-    sin = su.info(self.root, self.name).iloc[0]
-    if ids is None:
-        ids = range(1, sin.n_slides+1)
-    if resize is True:
-        resize = (sin.v_width, sin.v_height)
-    for si in ids:
-        sp = self._img_path(sp, si)
-        if self._is_valid_sid(si, sin.n_slides):
-          if gray:
-              img = cv2.imread(sp, cv2.COLOR_GRAY2BGR)
-          else:
-              img = cv2.imread(sp)
-        else:
-            img = self._blank_slide()
-            img = img[0] if gray else img
-        if resize is not None:
-            img = cv2.resize(img, resize)
-        yield(dict(img=img, idx=si))
-
-  def get_slide(self, index=1, resize=None):
-    su = Summary()
-    sin = su.info(self.root, self.name).iloc[0]
-    if resize is True:
-        resize = (sin.v_width, sin.v_height)
-    if self._is_valid_sid(index, sin.n_slides):
-        spb = self.slides_path('big')
-        img = cv2.imread(self._img_path(spb, index))
-    else:
-        img = self._blank_slide()
-    if resize is not None:
-        img = cv2.resize(img, resize)
-    return img[:, :, [2, 1, 0]]  # convert for matplotlib
