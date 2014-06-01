b5235e61994f279317f1cdca31327933d65928c6
speed-of-light <speed.of.lightt@gmail.com>
2014-06-01 14:40:01 +0800

    Add plotter base constructor #17

--- lib/plotter/base.py
+++ lib/plotter/base.py
@@ -2,5 +2,7 @@ class Plotter(object):
     """
     Plotter base
     """
-    def __init__(self):
-        pass
+    def __init__(self, root, name, data):
+        self.data = data
+        self.root = root
+        self.name = name
