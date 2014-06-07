0f2500d67ebed0ee600e0c2b08346389b5e92f7a
speed-of-light <speed.of.lightt@gmail.com>
2014-06-07 15:04:06 +0800

    Add annotation to ground truth plotter

--- lib/plotter/ground_truth_plotter.py
+++ lib/plotter/ground_truth_plotter.py
@@ -1,4 +1,54 @@
+import matplotlib.cm as mcm
 from base import Plotter
+
+
+class SegLocator(object):
+    def __init__(self, nth=0, height=10):
+        """
+        Get segment location information by given segment index and height
+        """
+        self.set_height(height)
+        self.nth = nth
+
+    def set_height(self, height):
+        self.height = height
+        self.__tag_at = height * 0.1
+        self.__margin = height * 0.2
+        self.__center = height * 0.5
+        self.__content = height * 0.6
+
+    def __brah_center(self, start, size):
+        return start + size/2.0
+
+    def __nth_base(self):
+        """
+        Return nth element of y-axis base position
+        """
+        return self.height * self.nth
+
+    def yrange(self):
+        """
+        Return the range of y, start from (seg height)*si + content height,
+            with height 6
+        """
+        return (self.__nth_base() + self.__margin,
+                self.__content)
+
+    def txt_pos(self, xinfo, tag_at=None):
+        """
+        xinfo: containing `start` and `size`, here use to get brah center
+        """
+        if tag_at is None:
+            tag_at = self.__tag_at
+        return (self.__brah_center(xinfo[0], xinfo[1]),
+                self.__nth_base() + tag_at)
+
+    def data_pos(self, xinfo):
+        """
+        xinfo: containing `start` and `size`, here use to get brah center
+        """
+        return (self.__brah_center(xinfo[0], xinfo[1]),
+                self.__nth_base() + self.__center)
 
 
 class GroundTruthPlotter(Plotter):
@@ -6,7 +56,7 @@ class GroundTruthPlotter(Plotter):
     Plot Ground truth segmented data
     """
     def __init__(self, segments):
-        self.seg = segments
+        self.sg_list = segments
 
     def plot(self, ax):
         """
@@ -14,18 +64,47 @@ class GroundTruthPlotter(Plotter):
         """
         pass
 
-    def segmentize(ax, segments_list):
+    def __ano_sid(self, ax, gnd, seg_locator, tag_at):
+      """
+      Add annotation to text with tag position
+      """
+      txt_pos = seg_locator.txt_pos(gnd, tag_at)
+      data_pos = seg_locator.data_pos(gnd)
+      arp = dict(facecolor=(.5, .9, .5), shrink=0.05)
+      ax.annotate(gnd[2], xy=data_pos, xytext=txt_pos,
+                  xycoords='data', textcoords='data',
+                  horizontalalignment='center',
+                  arrowprops=arp)
+
+    def __ano_sids(self, ax, gnds, seg_locator):
+        for gi, gnd in enumerate(gnds):
+            self.__ano_sid(ax, gnd, seg_locator, (gi % 3+1)*0.5)
+
+    def __seg_xy(self, segl):
+        mcm.Paired.N = 100
+        segs = [(dd[0], dd[1]) for dd in segl]
+        segv = [dd[-1] for dd in segl]
+        msv = max(segv)
+        segc = [mcm.Paired(cc*1.0/msv) for cc in segv]
+        return segs, segc
+
+    def segmentize(self, ax, base=0):
         """
         Add segmented plot for input ground truth data
         """
-        matplotlib.cm.hsv.N = 100
         max_x_lim = 0
-        for si, sg in enumerate(segments_list):
-            segs = [(dd[0], dd[1]) for dd in sg]
-            segv = [matplotlib.cm.Paired(dd[-1]/48.0) for dd in sg]
-            ax.broken_barh(segs, (10*si+2, 6), facecolors=segv)
-            max_x_lim = max(max_x_lim, (sg[-1][0]+sg[-1][1]))  # from last frame id plus duration
+        min_x_lim = 9999999
+        segl = SegLocator(height=10)
+        sglist = self.sg_list
+        for si, sg in enumerate(sglist):
+          segs, segv = self.__seg_xy(sg)
+          segl.nth = si + base
+          ax.broken_barh(segs, segl.yrange(), facecolors=segv)
+          self.__ano_sids(ax, sg, segl)
+          # from last frame id plus duration
+          max_x_lim = max(max_x_lim, (sg[-1][0]+sg[-1][1]))
+          min_x_lim = min(min_x_lim, sg[-1][0])
         # update global style
-        ax.set_ylim(0, 10*len(segments_list))  # set y max by value of y
-        ax.set_xlim(0, max_x_lim*1.01)
+        xlim = ax.get_xlim()
+        ax.set_xlim(xlim[0], max_x_lim*1.01)
         return ax
