8f084622f3fb452e6df862376a03826107d990f4
speed-of-light <speed.of.lightt@gmail.com>
2014-06-07 23:19:38 +0800

    Improve ploter information, close #53

--- lib/plotter/ground_truth/direct_match.py
+++ lib/plotter/ground_truth/direct_match.py
@@ -9,17 +9,24 @@ class GtDirectMatch(Plotter):
         labels = [l.get_label() for l in lns]
         ax.legend(lns, labels, loc=0)
 
-    def absolute_pairs(self, ax, pairs, top):
+    def __txy(self, ax, title, x, y):
+        ax.set_title(title)
+        ax.set_xlabel(x)
+        ax.set_ylabel(y)
+
+    def absolute_pairs(self, ax, pairs, top, name="Slide ID"):
         dy = [p for p in pairs.sid]
         dx = [p for p in pairs.fid]
-        ln = ax.plot(dx, dy, marker='x', label='Slide id')
-        bn = ax.axvline(top, 0, max(dy), color='b', ls='-.', label='bound')
-        lns = ln + [bn]
+        ln = ax.plot(dx, dy, marker='x', label=name)
+        ax.axvline(top, 0, max(dy), color='b', ls='-.', label='bound')
+        lns = ln
         self.__legend(ax, lns)
+        self.__txy(ax, "GroundTruth of Slide-Frame pairs", "Frame", "Slide")
 
-    def relative_pairs(self, ax, pairs):
+    def relative_pairs(self, ax, pairs, name="Slide ID"):
         dy = [p for p in pairs.sid]
         dx = range(len(dy))
-        lns = ax.plot(dx, dy, marker='x', label="Slide id")
+        lns = ax.plot(dx, dy, marker='x', label=name)
         ax.set_xlim(0, dx[-1])
         self.__legend(ax, lns)
+        self.__txy(ax, "GroundTruth of relative Slide-Frame pairs", "Time order", "Slide")
