4f987a5c2fdfdaa89b710d0c2a83227868ed7d05
speed-of-light <speed.of.lightt@gmail.com>
2014-06-08 23:51:09 +0800

    refactor exp.base

--- lib/exp/base.py
+++ lib/exp/base.py
@@ -1,9 +1,6 @@
 # system
 import os
-import re
 import shutil
-import logging
-import logging.handlers
 # scientific
 import pandas as pd
 # custom
@@ -22,37 +19,7 @@ class ExpCommon(object):
         self.upass = None
         self._init_logger()
 
-    def _init_logger(self):
-        cn = self._underscore(self.__class__.__name__)
-        fmt = logging. \
-            Formatter(fmt="%(asctime)s,%(levelname)s,\
-                      %(name)s,%(funcName)s,%(lineno)d, %(message)s",
-                      datefmt='%m/%d/%Y %H:%M:%S')
-        logger = logging.getLogger("{}.{}.{}.{}".
-                                   format(__name__, cn, self.root, self.name))
-        logger.setLevel(logging.INFO)
-        logger.propagate = 0
-        # FileHandler
-        fn = self.make_path('log', 'log', True, False)
-        fnh = fn + "_fh"
-        cnh = fn + "_ch"
-        if fnh not in [lh.name for lh in logger.handlers]:
-            fh = logging.handlers. \
-                RotatingFileHandler(fn, maxBytes=10485760, backupCount=5)
-            fh.name = fnh
-            fh.setFormatter(fmt)
-            logger.addHandler(fh)
-        # StreamHandler
-        if 0 and cnh not in [lh.name for lh in logger.handlers]:
-            ch = logging.StreamHandler()
-            ch.name = cnh
-            ch.setFormatter(fmt)
-            logger.addHandler(ch)
-        logger.info(">>============== {} inited ================= <<".
-                    format(cn))
-        self.log = logger
-
-    def _save_key(self, fpath, key):
+    def __save_key(self, fpath, key):
         kf = self.load('keys')
         if kf is None:
             kf = pd.DataFrame([key], columns=['key'])
@@ -65,13 +32,6 @@ class ExpCommon(object):
         kf.to_hdf(fpath, 'keys', mode='a', data_columns=True,
                   format='t', complib='blosc', complevel=self.comp)
 
-    def _underscore(self, string=''):
-        # move pre-compile out the loop to improve performance
-        first_cap_re = re.compile('(.)([A-Z][a-z]+)')
-        all_cap_re = re.compile('([a-z0-9])([A-Z])')
-        s1 = first_cap_re.sub(r'\1_\2', string)
-        return all_cap_re.sub(r'\1_\2', s1).lower()
-
     def _ustr_dict(self, di):
         """
         Make dict keys underscore for saving to hdfs
@@ -83,39 +43,10 @@ class ExpCommon(object):
             s += "/{}_{}".format(k, di[k])
         return s
 
-    def _asure_path(self, path):
-        if not os.path.exists(path):
-            os.makedirs(path)
-
     def store(self):
         sp = self.store_path()
         return pd.HDFStore(sp, format='t', data_columns=True,
                            complib='blosc', complevel=self.comp)
-
-    def make_path(self, resource='stores', ext='h5', asure=True, root=False):
-        """
-        resource: resource name in path
-        ext: file extension for resource, if None then return only path
-        asure: make sure the path exist
-        root: get only root path of given resource
-        usage:
-            # make simple log path, and make sure created
-            self.make_path('log', None)
-            # make path for log with log extension, and not check existence
-            self.make_path('log', 'log', False)
-        """
-        rt = self.root
-        pn = self.name
-        cn = self._underscore(self.__class__.__name__)
-        pth = "data/{}/{}/{}".format(rt, pn, resource)
-        if root:
-            return pth
-        if asure:
-            self._asure_path(pth)
-        pth = "{}/{}".format(pth, cn)
-        if ext is not None:
-            pth = "{}.{}".format(pth, ext)
-        return pth
 
     def store_path(self):
         """
@@ -141,7 +72,7 @@ class ExpCommon(object):
         self.log.info('save key [{}] to path: {}'.format(key, sp))
         data.to_hdf(sp, key, mode='a', data_columns=True, format='t',
                     complib='blosc', complevel=self.comp)
-        self._save_key(sp, key)
+        self.__save_key(sp, key)
 
     def load(self, key):
         sp = self.make_path(asure=False)
