7d1474e1624c32c74a0410ea34cd6547b2490e47
speed-of-light <speed.of.lightt@gmail.com>
2014-06-08 21:40:40 +0800

    Move doer to part of exp package

--- lib/exp/doer.py
+++ lib/exp/doer.py
@@ -1,18 +1,17 @@
 import numpy as np
-from exp.prepare import Prepare
-from exp.feats import Feats
-from exp.matching import Matcher
-from exp.evaluator.ground_truth import GroundTruth
+from base import ExpCommon
+from prepare import Prepare
+from matching import Matcher
+from evaluator.ground_truth import GroundTruth
 
 
-class Doer(object):
+class Doer(ExpCommon):
     def __init__(self, root, name):
         self.root = root
         self.name = name
-        self.feats = Feats(root, name)
-        self.matcher = Matcher(root, name)
 
     def __reduced_frames(self):
+        """ Get preprocessed frames """
         pp = Prepare(self.root, self.name)
         df = pp.load('keys')
         pk = df[df.key.str.contains('reduce')].key.values[5]
@@ -22,10 +21,15 @@ class Doer(object):
     def __gnd_mpairs(self):
         """
         load ground truth matched pairs
+        TODO change univ_df() to pairs()
         """
         gt = GroundTruth(self.root, self.name)
         df = gt.univ_df()
         return df
+
+    def __matching(self, fids, thres):
+        mm = Matcher(self.root, self.name)
+        res, sfs, vfs = mm.set_match(fids, ransac=False, thres=0.9)
 
     def rundown(self):
         pass
