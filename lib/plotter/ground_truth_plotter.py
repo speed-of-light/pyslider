76d127edb2129e6e4fc0f62dad860a3cb5359e47
speed-of-light <speed.of.lightt@gmail.com>
2014-06-11 16:38:29 +0800

    Add group pair method, resolve #65

--- lib/plotter/ground_truth_plotter.py
+++ lib/plotter/ground_truth_plotter.py
@@ -1,5 +1,4 @@
 import matplotlib.cm as mcm
-from base import Plotter
 from ground_truth.segments import GtSegments
 from ground_truth.direct_match import GtDirectMatch
 
@@ -13,3 +12,20 @@ class GroundTruthPlotter(GtSegments, GtDirectMatch):
         Here should put some plot configurations
         """
         GtSegments.__init__(self, cmap=mcm.Paired)
+
+    def group_pair(self, fig, kwargs):
+        """
+        kwargs:
+            `gnd_raw`: raw ground truth data
+            `info`: summary of each keys
+        """
+        ax = fig.add_subplot(111)
+        lns = []
+        for key in kwargs.keys():
+            df = kwargs[key]['gnd_raw']
+            top = kwargs[key]['info'].n_frames
+            ln = self.absolute_pairs(ax, df[df.sid > 0], top, key)
+            lns.append(ln[0])
+        self.set_legends(ax, lns)
+        self.set_titles(ax, "GroundTruth of Slide-Frame Pairs")
+        return fig
