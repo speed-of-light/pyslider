fcdfeb95e569126cd0863e0d91c9b663b00e70c2
speed-of-light <speed.of.lightt@gmail.com>
2014-06-11 16:07:38 +0800

    Fix for same axes in different loop

--- lib/plotter/ground_truth/direct_match.py
+++ lib/plotter/ground_truth/direct_match.py
@@ -5,23 +5,37 @@ class GtDirectMatch(Plotter):
     def __init__(self, **kwargs):
         Plotter.__init__(self, **kwargs)
 
-    def __legend(self, ax, lns):
-        labels = [l.get_label() for l in lns]
-        ax.legend(lns, labels, loc=0)
+    def set_legends(self, ax, handles):
+        """
+        Use to add legends for axes plotted with multiple data set
+        lhs: line handles of axes
+        """
+        labels = [h.get_label() for h in handles]
+        ax.legend(handles, labels, loc=0)
 
     def __txy(self, ax, title, x, y):
         ax.set_title(title)
         ax.set_xlabel(x)
         ax.set_ylabel(y)
 
-    def absolute_pairs(self, ax, pairs, top, name="Slide ID"):
+    def set_titles(self, ax, title="Slide - Frame pairs"):
+        """
+        Set x, y, labels and axes title
+        """
+        self.__txy(ax, title, "Frame", "Slide")
+
+    def absolute_pairs(self, ax, pairs, xtop, name="Slide ID"):
+        """
+        Plot ground truth data with line-wise pairs
+        top: the frame limit from a video
+        name:
+        """
         dy = [p for p in pairs.sid]
         dx = [p for p in pairs.fid]
         ln = ax.plot(dx, dy, marker='x', label=name)
-        ax.axvline(top, 0, max(dy), color='b', ls='-.', label='bound')
-        lns = ln
-        self.__legend(ax, lns)
-        self.__txy(ax, "GroundTruth of Slide-Frame pairs", "Frame", "Slide")
+        ax.axvline(xtop, 0, max(dy), color=ln[0].get_color(),
+                   ls='-.', label='bound', lw=2)
+        return ln
 
     def relative_pairs(self, ax, pairs, name="Slide ID"):
         dy = [p for p in pairs.sid]
@@ -29,4 +43,5 @@ class GtDirectMatch(Plotter):
         lns = ax.plot(dx, dy, marker='x', label=name)
         ax.set_xlim(0, dx[-1])
         self.__legend(ax, lns)
-        self.__txy(ax, "GroundTruth of relative Slide-Frame pairs", "Time order", "Slide")
+        self.__txy(ax, "GroundTruth of relative Slide-Frame pairs",
+                   "Time order", "Slide")
