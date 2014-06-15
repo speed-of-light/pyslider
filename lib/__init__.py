03ba8db41d0a4a0b8db236658d3373c8d9a4acfb
speed-of-light <speed.of.lightt@gmail.com>
2014-06-15 20:09:12 +0800

    Add doc string

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,1 +1,8 @@
+__doc__ = """
+    Pyslider library
+    `exp`: core part
+    `auto`: some auto scripts
+    `plotter`: plotting library
+    `ext`: some helpers provided by others
+"""
 __all__ = ["exp"]
