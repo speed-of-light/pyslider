97dfe4910cfd95c2a4874afb2036bd6d9d215ebf
speed-of-light <speed.of.lightt@gmail.com>
2014-06-16 01:15:52 +0800

    Add dataset class

--- lib/__init__.py
+++ lib/__init__.py
@@ -6,3 +6,22 @@ __doc__ = """
     `ext`: some helpers provided by others
 """
 __all__ = ["exp"]
+
+from tools.emailer import Emailer
+
+
+class Dataset(object):
+    def __init__(self, root, name):
+        self.root = root
+        self.name = name
+
+    def notify(self, summary):
+        if self.upass is None:
+            return
+        ps = self.upass
+        cn = self.underscore(self.__class__.__name__)
+        title = "Pyslider Job: {} <{}-{}> Finished". \
+            format(cn, self.root, self.name)
+        me = "speed.of.lightt@gmail.com"
+        with Emailer(config=dict(uname=me, upass=ps)) as mailer:
+            mailer.send(title, summary, ['speed.of.lightt@gmail.com'])
