1191fb368cbfaf010a59c5aee0127cc0686e6396
speed-of-light <speed.of.lightt@gmail.com>
2014-06-16 18:15:33 +0800

    Refactor `slider`, remove `get_slide` method

--- lib/exp/tools/slider.py
+++ lib/exp/tools/slider.py
@@ -3,15 +3,14 @@ from ..summary import Summary
 from pdf_reader import PdfReader
 
 
-class Slider(object):
+class Slider(PdfReader):
     """
     A helper to get slide information
     """
     def __init__(self, root="", name=""):
-        self.root = root
-        self.name = name
+        PdfReader.__init__(self, root, name)
 
-    def _blank_slide(self):
+    def __blank_slide(self, gray=False):
         sp = self.slides_path()
         img = cv2.imread("{}/{:03d}.jpg".format(sp, 1))
         img[:] = (255, 255, 255)  # fill white
@@ -24,57 +23,54 @@ class Slider(object):
         botL = (img.shape[1], 0)
         cv2.line(img, topL, botR, red, line_width)
         cv2.line(img, topR, botL, red, line_width)
-        return img
+        if gray:
+            return img[0]
+        else:
+            return img
 
-    def _is_valid_sid(self, index, count):
+    def __is_valid_sid(self, index, count):
         return (index > 0 and index < count+1)
 
-    def _img_path(self, root, idx):
+    def __img_path(self, root, idx):
         return "{}/{:03d}.jpg".format(root, idx)
 
+    def __info(self):
+        su = Summary()
+        sin = su.info(self.root, self.name)
+        return sin
+
     def slide_pages(self):
-        ps = Slider(self.root, self.name)
+        ps = PdfReader(self.root, self.name)
         return ps.pages()
 
     def slides_path(self, size='mid'):
         ps = PdfReader(self.root, self.name)
         return ps.slides_path(size)
 
+    def __make_img(self, idx, itop, path, gray):
+        if self.__is_valid_sid(idx, itop):
+            if gray:
+                img = cv2.imread(path, cv2.COLOR_GRAY2BGR)
+            else:
+                img = cv2.imread(path)
+        else:
+            img = self.__blank_slide(gray)
+        return img
+
     def get_slides(self, ids=[], gray=False, resize=None):
         """
         Get slide images collection
+        use img[:, :, [2, 1, 0]] to convert for matplotlib
         """
         sp = self.slides_path(size='big')
-        su = Summary()
-        sin = su.info(self.root, self.name)
+        sin = self.__info()
         if ids is None:
             ids = range(1, sin.n_slides+1)
         if resize is True:
             resize = (sin.v_width, sin.v_height)
         for si in ids:
-            sp = self._img_path(sp, si)
-            if self._is_valid_sid(si, sin.n_slides):
-                if gray:
-                    img = cv2.imread(sp, cv2.COLOR_GRAY2BGR)
-                else:
-                    img = cv2.imread(sp)
-            else:
-                img = self._blank_slide()
-                img = img[0] if gray else img
+            sp = self.__img_path(sp, si)
+            img = self.__make_img(si, sin.n_slides, sp, gray)
             if resize is not None:
                 img = cv2.resize(img, resize)
             yield(dict(img=img, idx=si))
-
-    def get_slide(self, index=1, resize=None):
-        su = Summary()
-        sin = su.info(self.root, self.name).iloc[0]
-        if resize is True:
-            resize = (sin.v_width, sin.v_height)
-        if self._is_valid_sid(index, sin.n_slides):
-            spb = self.slides_path('big')
-            img = cv2.imread(self._img_path(spb, index))
-        else:
-            img = self._blank_slide()
-        if resize is not None:
-            img = cv2.resize(img, resize)
-        return img[:, :, [2, 1, 0]]  # convert for matplotlib
