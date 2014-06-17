5401e723a436ea1b0b3a944de442e18c47666f05
speed-of-light <speed.of.lightt@gmail.com>
2014-06-17 23:30:07 +0800

    Add get_feats_pair

--- lib/exp/pre/__init__.py
+++ lib/exp/pre/__init__.py
@@ -22,8 +22,8 @@ class Reducer(ExpCommon):
         Get the compressed dataset and compressed ratio
         """
         before = len(data)
-        igmax = data.iloc[0].index
-        last_v = data.iloc[0].index
+        igmax = data.iloc[0].name
+        last_v = data.iloc[0].name
         for v in data.index[1:]:
             if (v - last_v) == 1:  # check conti series max
                 if data.ix[igmax]['diff'] < data.ix[v]['diff']:
@@ -41,21 +41,27 @@ class Reducer(ExpCommon):
         Get probed `differenc` values and `frame` data
         """
         df = self.load(key)
-        dmean = df.diff.mean()
-        can = df[df.diff.gt(dmean)]
-        return can
+        dmean = df["diff"].mean()
+        can = df[df["diff"].gt(dmean)]
+        return can, len(df)
 
     def __results(self, **opts):
-        after = opts["after"]
-        before = opts["before"]
-        info = "before: {}, after: {}, ratio: {}".\
-            format(before, after, after*1.0/before)
-        print "Reduce data from", info
-        self.elog.info("Reduce data from", info)
+        oo = opts["origin"]
+        ff = opts["final"]
+        info = "Original: {}, thresed: {}, final: {}, ratio: {}".\
+            format(oo, opts["thresed"], ff, ff*1.0/oo)
+        istr = "Reduce data from {}".format(info)
+        print istr
+        self.elog.info(istr)
         self.save_rtlog(opts.keys(), opts.values())
 
     def reduce(self, key):
-        can = self.__get_data(key)
-        red, before, after = self.__compress(can)
-        self.__results(key=key, before=before, after=after)
+        can, orl = self.__get_data(key)
+        red, thresed, after = self.__compress(can)
+        self.__results(key=key, origin=orl,
+                       thresed=thresed, final=after)
         return red
+
+    def clear(self):
+        self.delete_log()
+        # self.delete_store()  # be careful
