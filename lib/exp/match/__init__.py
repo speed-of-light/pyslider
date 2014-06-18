eb872ba95a932f54cc14a1e539304d361bbb080f
speed-of-light <speed.of.lightt@gmail.com>
2014-06-18 23:50:29 +0800

    Add in memory matching

--- lib/exp/match/__init__.py
+++ lib/exp/match/__init__.py
@@ -27,6 +27,62 @@ class Matchx(ExpCommon, Mahelp):
         self.klass_var = st
         self.elog.info("Current configs: {}".format(st))
 
+    def __matching(self, matcher, sk=None, fk=None, thres=0.9):
+        mra = []
+        with ExpTimer(verbose=0) as ts:
+            if len(fk) > 0:
+                mra = matcher.knnMatch(sk.values, fk.values, k=2)
+                mra = self._remove_high_simi(mra, thres)
+        mdf = self._to_df(mra)
+        return mdf, ts
+
+    def __save_rtlog(self, pairs=None, lens=None, info=None):
+        pairs.extend(lens)
+        pairs.extend(info)
+        self.save_rtlog(self._rtlog_cols(), pairs)
+
+    def __match_info(self, mat, time, fxp):
+        data = self._match_info(mat, time.msecs, fxp)
+        self.elog.info(self._info_str(**data))
+        self.__save_rtlog(**data)
+
+    def __match_mem(self, **opts):
+        self.matches.append(opts)
+
+    def __match_info_mem(self, mat, time, fxp):
+        """
+        In memory verison of __match_info
+        """
+        data = self._match_info(mat, time.msecs, **fxp)
+        self.elog.info(self._info_str(**data))
+        self._mem_rtlog(**data)
+
+    def __save_match(self, sid, fid, df):
+        self.save(self._pair_key(sid, fid), df)
+
+    def __match(self, sids, fids, thres=0.8):
+        """
+        Save matched result by `keys` formated as:
+            `m_{sid:03d}_{fid:03d}`
+        """
+        ma = cv2.DescriptorMatcher_create(self.mcore)
+        sfs = self.fx.load_slides_feats(sids)
+        for fid in fids:
+            fk = self.fx.load_frame_feats(fid)
+            for sid, sk in zip(sids, sfs):
+                fxp = dict(sk=sk, fk=fk, thres=thres)
+                df, optime = self.__matching(ma, **fxp)
+                fxp.update(dict(sid=sid, fid=fid))
+                self.__match_info(df, optime, fxp)
+                self.__save_match(sid, fid, df)
+
+    def __match_core(self, ma, **opts):
+        fxp = dict(sk=opts["sk"], fk=opts["fk"])
+        df, optime = self.__matching(ma, **fxp)
+        fxp.update(dict(fid=opts["fid"], sid=opts["sid"]))
+        self.__match_info_mem(df, optime, fxp)
+        self.__match_mem(sid=opts["sid"], fid=opts["fid"], df=df)
+
     def set_featx(self, engine="keypoint", method="SIFT"):
         """
         Refer to Featx.set_algorithm for more info
@@ -44,46 +100,19 @@ class Matchx(ExpCommon, Mahelp):
         self.elog.info("Use matching core: {}".format(fn))
         self.__klass_var()
 
-    def __matching(self, feats, matcher, thres):
-        mra = []
-        with ExpTimer(verbose=0) as ts:
-            if len(feats["fk"]) > 0:
-                mra = matcher.knnMatch(feats["sk"].values,
-                                       feats["fk"].values, k=2)
-                mra = self._remove_high_simi(mra, thres)
-        mdf = self._to_df(mra)
-        return mdf, ts
-
-    def __save_rtlog(self, pairs=None, lens=None, info=None):
-        cols = ["sid", "fid", "skcnt", "fkcnt", "mrcnt",
-                "mean_dist", "ssc", "fsc", "time"]
-        pairs.extend(lens)
-        pairs.extend(info)
-        self.save_rtlog(cols, pairs)
-
-    def __match_info(self, sid, fid, fxp, mat, time):
-        data = self._match_info(sid, fid, fxp, mat, time.msecs)
-        self.elog.info(self._info_str(**data))
-        self.__save_rtlog(**data)
-
-    def __save_match(self, sid, fid, df):
-        self.save("m_{:03d}_{:03d}".format(sid, fid), df)
-
-    def __match(self, sids, fids, thres=0.8):
-        """
-        Save matched result by `keys` formated as:
-            `m_{sid:03d}_{fid:03d}`
-        """
-        ma = cv2.DescriptorMatcher_create(self.mcore)
-        sfs = self.fx.load_slides_feats(sids)
-        for fid in fids:
-            fk = self.fx.load_frame_feats(fid)
-            for sid, sk in zip(sids, sfs):
-                fxp = dict(sk=sk, fk=fk)
-                df, optime = self.__matching(fxp, ma, thres)
-                self.__match_info(sid, fid, fxp, df, optime)
-                self.__save_match(sid, fid, df)
-
     def match(self, thres=0.8):
         sids, fids = self.seeds()
         self.__match(sids, fids, thres)
+
+    def single_match(self, fid, thres=0.9):
+        """
+        Try to find matches on a single frame
+        Check `self.rtlog` and `matches` as result
+        """
+        self.matches = []
+        ma = cv2.DescriptorMatcher_create(self.mcore)
+        sids = self.slide_seeds()
+        sfs = self.fx.load_slides_feats(sids)
+        fk = self.fx.load_frame_feats(fid)
+        for sid, sk in zip(sids, sfs):
+            self.__match_core(ma, thres=thres, sid=sid, fid=fid, sk=sk, fk=fk)
