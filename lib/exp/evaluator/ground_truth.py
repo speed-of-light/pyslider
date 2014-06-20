b33e51e0b7791f2fbaac03565845c116698cc47c
speed-of-light <speed.of.lightt@gmail.com>
2014-06-21 02:47:02 +0800

    Fix bug in answer while no matches in df

--- lib/exp/evaluator/ground_truth.py
+++ lib/exp/evaluator/ground_truth.py
@@ -140,13 +140,15 @@ class GroundTruth(ExpCommon, Summary):
         Get sid by given fid
         """
         df = self._preload("segments")
-        dfi = df[df.fstart <= fid].iloc[-1]
-        fs = dfi.fstart
-        fe = dfi.fstart + dfi.duration
-        if fs <= fid <= fe:
-            return dfi.sid
-        else:
-            return -1
+        dfi = df[df.fstart <= fid]
+        ret = -1
+        if len(dfi) > 0:
+            dfi = dfi.iloc[-1]
+            fs = dfi.fstart
+            fe = dfi.fstart + dfi.duration
+            if fs <= fid <= fe:
+                ret = dfi.sid
+        return ret
 
     def guess(self, fid, sid):
         """
