62dfc5f9f4425adbe7517c2538e2ad74082a8cbc
speed-of-light <speed.of.lightt@gmail.com>
2014-07-06 01:09:14 +0800

    Using 'eps' as default format

--- lib/plotter/plot_filer.py
+++ lib/plotter/plot_filer.py
@@ -29,10 +29,10 @@ class _PlotFiler(object):
         self.__assert_name_exist(name)
         ctype = self.__classname()
         args = dict(ft=ft, ctype=ctype, method=method)
-        base = "data/fig/{ft}/{ctype}/{method}"
+        base = "data/fig/{ctype}/{method}"
         root = base.format(**args)
         self.__asure_path(root)
-        return "{}/{}.svg".format(root, name)
+        return "{}/{}.{}".format(root, name, ft)
 
     def __underscore(self, string=""):
         first_cap_re = re.compile("(.)([A-Z][a-z]+)")
@@ -43,6 +43,6 @@ class _PlotFiler(object):
     def _savefig(self, figh, func, name):
         if not self.autosave_fig:
             return
-        fp = self.__make_path(ft="svg", method=func, name=name)
-        figh.savefig(fg, frame_on=0)
+        fp = self.__make_path(ft="eps", method=func, name=name)
+        figh.savefig(fp, transparent=1)
         print "Figure saved to {}".format(fp)
