9ee3a3b34533bf8c0a81f82836713a797bb162dc
speed-of-light <speed.of.lightt@gmail.com>
2014-07-23 20:37:54 +0800

    add mater to plot paired frame slide pair mat within dist

--- lib/plotter/xframes/__init__.py
+++ lib/plotter/xframes/__init__.py
@@ -6,6 +6,7 @@ from lib.plotter.plot_filer import _PlotFiler as PF
 from roc_plot import _RocPlot as Rocp
 from roc_grid import _RocGrid as Rgrid
 from frames import _Frames as Frs
+from mater import _Mater as Mater
 
 
 class XframePlotter(PF):
@@ -22,12 +23,17 @@ class XframePlotter(PF):
     def __rn(self, more=""):
         return self.xfe.root + "_" + self.xfe.name + more
 
+    def __red_ary(self, ary):
+        return reduce(lambda x,y: "{},{}".format(x, y), ary)
+
     def __plot_base(func):
         @wraps(func)  # enable func.__name__
         def inner(self, *args, **kwargs):
             func(self, *args, **kwargs)
             more = "_s{}".format(kwargs["st"]) if "st" in kwargs else ""
-            PF._savefig(self, kwargs["fig"], func.__name__, self.__rn(more=more))
+            more += "_o{}".format(self.__red_ary(kwargs["obk"])) if "obk" in kwargs else ""
+            PF._savefig(self, kwargs["fig"], func.__name__,
+                        self.__rn(more=more))
             # return data
         return inner
 
@@ -50,3 +56,12 @@ class XframePlotter(PF):
     def ns_frame_list(self, fig=None, data=None, st=0):
         pt = Frs(self.__ds_key(), self.xfe.root, self.xfe.name)
         pt.plot(fig, data, st=st)
+
+    @__plot_base
+    def dist_mat(self, fig=None, data=None, st=0, obk=[0,1]):
+        """
+        st: starting point of data
+        obk: observing key
+        """
+        pt = Mater(self.__ds_key(), self.xfe.gnd.info().n_slides, obk)
+        pt.plot(fig, data, st)
